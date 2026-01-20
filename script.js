af0995bac64cfa66a6d86c5555ab3e7cfdee5c0d
Frederick Elledge <frederickjye@gmail.com>
2026-01-20 20:20:15 +0000

    update app script back to original

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ loadHTML("footer", "footers.html", () => {
 
 const form = document.getElementById("contactForm");
 const SCRIPT_URL =
-  "https://script.google.com/macros/s/AKfycbz1xoqFXzetr_wp-r89YAvjLQ3Y_hD5cnMtcvT3nU0jRkkhfBnA0y0WEg1AxqOHUEm2/exec";
+  "https://script.google.com/macros/s/AKfycbz-kpQRiJtx_DocYOKYuXA0j1kjfgMZkNeZd29poHkwLISF-4NUgBBcnu9tNiu8PZnlVw/exec";
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
