7ede3ed823cd7f7519a88c3549148d182f200071
Frederick Elledge <frederickjye@gmail.com>
2026-05-13 14:28:45 +0100

    add fade in and out effect on brand and intro text

--- js/base.js
+++ js/base.js
@@ -49,4 +49,19 @@ loadHTML("footer", "partials/footers.html", () => {
 });
 
 
+const fadeElements = document.querySelectorAll(".fade-text");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    } else {
+      entry.target.classList.remove("visible");
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+fadeElements.forEach(el => observer.observe(el));
 
